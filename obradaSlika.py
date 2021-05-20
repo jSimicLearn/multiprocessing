72400bbf0b4835ee2991efb47842fde992cbae19
Jovo <jovo.simic@apeiron-edu.eu>
2021-05-20 13:08:34 +0200

    init

---
+++ obradaSlika.py
@@ -0,0 +1,36 @@
+import time
+import concurrent.futures
+from PIL import Image, ImageFilter
+
+img_names = [
+    'slika1.jpg',
+    'slika2.jpg',
+    'slika3.jpg',
+    'slika4.jpg',
+    'slika5.jpg',
+    'slika6.jpg',
+    'slika7.jpg',
+    'slika8.jpg',
+]
+
+t1 = time.perf_counter()
+
+
+
+
+def obradi_sliku(img_name):
+    img = Image.open(img_name)
+
+    img = img.filter(ImageFilter.GaussianBlur(15))
+
+    img.save(f'obradjene/{img_name}')
+    print(f'{img_name} je obradjena...')
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    executor.map(obradi_sliku, img_names)
+
+
+t2 = time.perf_counter()
+
+print(f'Zavrseno u {t2-t1} sekundi')
